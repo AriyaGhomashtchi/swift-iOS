e7bef651ebbb2baa152588bcc9db58772fdf492d
Ariya Ghomashtchi <ariya_ghomashtchi@yahoo.com>
2023-04-24 13:29:42 +0200

    Just playing around Xcode to get familiar with the Tools.

--- Xcode First Time/ContentView.swift
+++ Xcode First Time/ContentView.swift
@@ -9,23 +9,53 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        
         // Vertical Stack Layout
-        VStack {
-            Image("fc arsenal")
-                .resizable()
-                .frame(width: 400.0, height: 300.0)
-                .cornerRadius(20)
-            Text("FC Arsenal")
-                .bold()
-                .padding(.top, -60.0)
+        ZStack {
+            Color("arsenal").ignoresSafeArea()
+            VStack {
+                VStack{
+                    Image("fc arsenal")
+                        .resizable(resizingMode:.tile)
+                        .frame(width: 400.0, height: 220.0)
+                        .cornerRadius(20)
+                        .padding(.top,-50)
+                                            
+                    
+                    Text("FC Arsenal")
+                    .bold()
+                    .padding()
+                    .font(.custom("Bradley Hand", size: 50))
+                    
+                }
+                Spacer()
+                Button("Click for Liveticker") {
+                    /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                }
+                .font(.system(size: 30))
+                Spacer()
+                
+                VStack{
+                    Image("chelsea")
+                        .resizable()
+                        .frame(width: 150.0, height: 150.0)
+                        .cornerRadius(50)
+                        .padding()
+                    Text("FC Chelsea")
+                        .font(.custom("Copperplate", size: 50))
+                        .bold()
+                       
+                    
+                }
+                .padding()
+            }
+            
         }
-        .padding()
+        
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
 }
